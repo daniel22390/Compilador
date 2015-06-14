7ab03fb5c639b597e1c0ce4ca27a37890b952278
daniel22390 <daniel22390@hotmail.com>
2015-06-14 02:20:56 -0300

    continuar verificaTipoVariavel (peguei o tipo do primeiro elemento) e tenho q verificar os outros

--- Compiladof/src/OCompilador/AnaliseSemantica.java
+++ Compiladof/src/OCompilador/AnaliseSemantica.java
@@ -6,7 +6,6 @@
 package OCompilador;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Stack;
@@ -107,22 +106,97 @@ public class AnaliseSemantica {
     }
 
     public void escopoFuncao(ArrayList<Lexema> escopo) {
-
+        // nao esquecer de  inserir o tipo do retorno na pilha
     }
 
     //retorna int, float, booleano(posicao contem a localizacao do id)
     public String verificaTipoVariavel(int posicao, ArrayList<Lexema> tokens) {
+        posicao = posicao + 2;
+        int i = posicao;
+        String tipoInicial = "";
+        boolean achou = false;
+        while(tokens.get(i).getTipo().equals("(")){
+            i++;
+        }
+        // se o primeiro elemento for id, ou fun
+        if (tokens.get(i).getTipo().equals("id") || tokens.get(i).getTipo().equals("fun")) {
+            for (ArrayList<Lexema> lista : varEscopo) {
+                for (Lexema lista1 : lista) {
+                    if (tokens.get(i).getNome().equals(lista1.getNome())) {
+                        tipoInicial = lista1.getTipo();
+                        achou = true;
+                        break;
+                    }
+                }
+                if (achou == true) {
+                    break;
+                }
+            }
+        } // se o primeiro elemento for string ou booleano
+        else if (tokens.get(i).getTipo().equals("String") || tokens.get(i).getTipo().equals("true") || tokens.get(i).getTipo().equals("false")) {
+            tipoInicial = tokens.get(i).getTipo();
+        } // se for float ou int, inicializo como num
+        else if(tokens.get(i).getTipo().equals("Int") || tokens.get(i).getTipo().equals("Float")){
+            tipoInicial = "num";
+        }
+        // analiso os prozimos tokens
+        while (!tokens.get(posicao).getTipo().equals("|n")) {
+            // nao esquecer, que tem q excluir todos os parametros de funcao e o que esta dentro das chaves do vetor
+            posicao++;
+        }
         return "";
     }
 
     //retorna vetor, matriz, id, fun(posicao contem a localizacao do id, o ] do vetor, ou id[), retorna vetor, matriz, id, fun
     public String verificaIdentificador(int posicao, ArrayList<Lexema> tokens) {
-        return "";
+        // se é o final de um vetor
+        if (tokens.get(posicao).getTipo().equals("]")) {
+            while (!tokens.get(posicao).getNome().equals("[")) {
+                posicao--;
+            }
+            posicao--;
+            if (tokens.get(posicao).getTipo().equals("]")) {
+                return "matriz";
+            } else {
+                return "vetor";
+            }
+        } // se ler um id
+        else {
+            // retorna vetor ou matriz
+            if (tokens.get(posicao + 1).getTipo().equals("[")) {
+                posicao++;
+                while (!tokens.get(posicao).getTipo().equals("]")) {
+                    posicao++;
+                }
+                posicao++;
+                if (tokens.get(posicao).getTipo().equals("[")) {
+                    return "matriz";
+                } else {
+                    return "vetor";
+                }
+            } //retorna funcao
+            else if (tokens.get(posicao).getTipo().equals("fun")) {
+                return "fun";
+            } //retorna id
+            else {
+                return "id";
+            }
+        }
     }
 
     // retorna o nome do vetor ou matriz (é passado a localização do ] final)
     public String nomeVetor(int posicao, ArrayList<Lexema> tokens) {
-        return "";
+        while (!tokens.get(posicao).getNome().equals("[")) {
+            posicao--;
+        }
+        posicao--;
+        if (tokens.get(posicao).getTipo().equals("]")) {
+            while (!tokens.get(posicao).getNome().equals("[")) {
+                posicao--;
+            }
+            posicao--;
+        }
+        return tokens.get(posicao).getNome();
     }
 
     public void escopoProg(ArrayList<Lexema> escopo) {
